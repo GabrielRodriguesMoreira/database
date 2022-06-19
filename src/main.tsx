d4af28a2e2f7b49987e8e3041e8b25614525fccd
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-06-19 11:22:56 -0300

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import API from '../components/API'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
