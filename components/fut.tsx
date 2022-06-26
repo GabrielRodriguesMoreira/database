662737f60e51f5fe0bc598593fc9f181e3090ee6
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-06-26 19:17:51 -0300

    Update fut.tsx

--- components/fut.tsx
+++ components/fut.tsx
@@ -5,8 +5,9 @@ import '../styles/fut.css'
 
 
 const apiSecret = import.meta.env.VITE_APP_CONVERTKIT_API_SECRET;
+
+console.log(apiSecret);
 const options = {
-    
 	method: 'GET',
 	headers: {
 		'Authorization': `Bearer ${apiSecret}`
