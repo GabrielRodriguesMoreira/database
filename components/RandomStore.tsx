ae70cce09f178e4e9cee0cd7cf02fea3cf64b36d
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-06-28 22:41:57 -0300

    RandomStore creation

--- components/RandomStore.tsx
+++ components/RandomStore.tsx
@@ -6,7 +6,7 @@ const apiSecret2 = import.meta.env.VITE_APP_IMGSPALASH_API_SECRET;
 export function RandomStore(){
 
     function createitem(){
-        fetch(`https://api.unsplash.com/photos/?client_id=${apiSecret2}`)
+        fetch(`https://api.unsplash.com/photos/random/?client_id=${apiSecret2}`)
         .then(response => response.json())
         .then( response => {
             console.log(response)
@@ -17,20 +17,21 @@ export function RandomStore(){
                 let productname = document.createElement('p');
                 let productprice = document.createElement('p');
                 productdiv.setAttribute('class','product_container')
-                productname.textContent = response?.[0].alt_description;
+                productname.textContent = 'robson';
                 productprice.textContent = String((Math.random() * 10000).toFixed(2));
-                imgproduct.setAttribute('src',response?.[0]?.['urls']?.small)
+                imgproduct.setAttribute('src',response?.['urls']?.small)
                 console.log(productname);
                 productdiv.append(imgproduct, productname, productprice);
-                storeparent.appendChild(productdiv)
+                storeparent.insertBefore(productdiv, storeparent.firstChild)
             }
         })
     }
 
     return(
         <div className='main_store_container'>
-            <div id='storeparent'>
                 <button onClick={createitem}>click-me</button>
+            <div id='storeparent'>
+
             </div>
         </div>
     )
