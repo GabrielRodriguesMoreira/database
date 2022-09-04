2fefd56bd611a4f17f8018f98c389e1d9c285b55
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-09-04 14:18:19 -0300

    init

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosRequestConfig } from 'axios'
+
+export function useAxios(url, options) {
+
+    const [data, setData] = useState(null)
+    const [isFetch, setFetch] = useState(true)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        axios.get(url, options)
+            .then(response => {
+                setData(response.data)
+            })
+            .catch(err => {
+                setError(err);
+            })
+            .finally(() => { setFetch(false) })
+
+
+    }, []);
+
+    return { data, error, isFetch }
+}
+
