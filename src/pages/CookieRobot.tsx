9da49aa0f86c9f8964a0c3c2309976290d29e17a
GabrielRodriguesMoreira <gabriel.rodrigues.moreira08@aluno.ifce.edu.br>
2022-12-02 13:59:48 -0300

    projeto1

--- src/pages/CookieRobot.tsx
+++ src/pages/CookieRobot.tsx
@@ -1,12 +1,24 @@
 import "../styles/guessgame.css"
-import { useEffect } from "react"
+import { createElement, useEffect, useState } from "react"
 
 export function CookieRobot(){
+    const [chances,setchances] = useState(0);
     var name = 'Naruto'
+    var lineuseds = [4]
+    var blocksuseds =[5]
+
+    useEffect(() => {
+        //funcao apertar enter
+        document.querySelector('#response')!.addEventListener('keypress', function (e:KeyboardEventInit) {
+                if (e.key === 'Enter') {
+                cleanblock()
+                }
+            });
+          },[]);
+
 
     function cleanblock(){
-        let lineuseds = [4]
-        let blocksuseds =[5]
+
 
         //deleteblock
         let elem = (document.getElementById("guessgame_image_container") as ParentNode);
@@ -17,33 +29,45 @@ export function CookieRobot(){
             blockindex = Math.floor(Math.random() * 8);
         }
 
-        
         let response = (document.getElementById("response") as HTMLInputElement);
 
-        //acertar
-        if(response.value.toUpperCase() == name.toUpperCase()){
-            for(let i=0; i<=5;i++){
-                elem?.removeChild(elem?.firstChild!);
+    if(chances<8){
+            //acertar
+                //limpar imagem
+            if(response.value.toUpperCase() == name.toUpperCase()){
+                for(let i=0; i<=5;i++){
+                    elem?.removeChild(elem?.firstChild!);
+                }
+
+                // desativar funcoes
+                response.setAttribute("disabled",'disabled');
+                let button = (document.getElementById("guessgame_button") as HTMLButtonElement)
+                button.setAttribute("disabled",'disabled');
+
+            } else {
+                //errar
+                //apagar blocos
+                lineuseds.push(lineindex);
+                blocksuseds.push(blockindex);
+                let line = elem?.childNodes[lineindex];
+                let block = line?.childNodes[blockindex];
+                (block as HTMLElement).style.background = 'transparent'
+
+                //adicionar historico de respostas
+                let parent = document.getElementById("history_list")
+                let child = document.createElement("li")
+                child.setAttribute("className", "guessgame_history_text")
+                child.innerHTML = response.value;
+                parent?.appendChild(child)
+
+                //adicionar contador
+                setchances(chances+1);
             }
-        } else {
-            lineuseds.push(lineindex);
-            blocksuseds.push(blockindex);
-            let line = elem?.childNodes[lineindex];
-            let block = line?.childNodes[blockindex];
-            (block as HTMLElement).style.background = 'transparent'
-        }
+    }
+        //limpar input
+        response.value = '';
 
     }
-
-
-    document.addEventListener('keydown', function(event){
-        if(event.key == "Enter"){
-            console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`)
-            cleanblock()
-        }
-
-    })
-
 
 
 
@@ -114,11 +138,17 @@ export function CookieRobot(){
                         <img src="https://tionitroblog.files.wordpress.com/2017/05/46e3b8b4cfec59be08b7b262ae7f611b.jpg" alt="" />
                 </div>
             </section>
-            <div className="guessgame_form">
-                    <input type="text" id='response'/>
-                    <button onClick={cleanblock}>Submit</button>
-                    
+            <div className="guessgame_responses">
+                <div className="guessgame_form">
+                    <input type="text" id='response' />
+                    <div className="guessgame_butto_counts">
+                        <h2>{chances}/6</h2>
+                        <button onClick={cleanblock} id='guessgame_button'>Submit</button>
+                    </div>
                 </div>
+                <ul className="history_list" id="history_list">
+                </ul>
+            </div>
         </div>
     )
 }
